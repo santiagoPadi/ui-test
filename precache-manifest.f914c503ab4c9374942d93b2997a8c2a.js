d73d0c8468754a9379ec903b7f274d2a33e6e294
santiago <santipachon4@gmail.com>
2020-09-20 13:53:08 -0500

    Updates

---
+++ precache-manifest.f914c503ab4c9374942d93b2997a8c2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9fde3ccd65890f1d5a032ed410f52e",
+    "url": "/ui-test/index.html"
+  },
+  {
+    "revision": "c19fa24a3904364549c3",
+    "url": "/ui-test/static/css/main.0a521d87.chunk.css"
+  },
+  {
+    "revision": "15ae9fda1fd13dad41b6",
+    "url": "/ui-test/static/js/2.804d01dd.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/ui-test/static/js/2.804d01dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19fa24a3904364549c3",
+    "url": "/ui-test/static/js/main.86c61d76.chunk.js"
+  },
+  {
+    "revision": "66667658396947b02964",
+    "url": "/ui-test/static/js/runtime-main.3fb78eb7.js"
+  },
+  {
+    "revision": "8ba148adba6caee304462d242300511f",
+    "url": "/ui-test/static/media/anyoneElse.8ba148ad.png"
+  },
+  {
+    "revision": "9dfd23d3ce92caac82fbed92a62ba5dc",
+    "url": "/ui-test/static/media/pope.9dfd23d3.png"
+  }
+]);
